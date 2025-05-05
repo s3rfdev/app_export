5f6b5837ebc804ec31c432582ec0d570099248bf
Sergei Fomenko <s3rf.im@gmail.com>
2025-05-05 15:14:42 +0500

    fox 1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1717474876"
+    serviceWorkerVersion: "1599218917"
   }
 });
